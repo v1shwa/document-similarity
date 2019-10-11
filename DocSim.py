f4509fc4e129dd089be4bef034bdcc3ec9c4467c
khinezarthwe <koalakhine@gmail.com>
2019-10-12 01:12:44 +0900

    #add-test - add type hint for vectorize function

--- DocSim.py
+++ DocSim.py
@@ -5,8 +5,12 @@ class DocSim(object):
         self.w2v_model = w2v_model
         self.stopwords = stopwords
 
-    def vectorize(self, doc):
-        """Identify the vector values for each word in the given document"""
+    def vectorize(self, doc: str) -> np.ndarray:
+        """
+        Identify the vector values for each word in the given document
+        :param doc:
+        :return:
+        """
         doc = doc.lower()
         words = [w for w in doc.split(" ") if w not in self.stopwords]
         word_vecs = []
